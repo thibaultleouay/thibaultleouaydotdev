9a9524370bf3fa69656155646bfbb75548fc6a9b
Thibault Le Ouay <thibaultleouay@gmail.Com>
2024-05-22 22:05:25 +0200

    ✏️ typo

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Thibault Le Ouay';
-export const SITE_DESCRIPTION = 'Thibault Le Ouay';
+export const SITE_TITLE = 'Thibault Le Ouay - Ducasse';
+export const SITE_DESCRIPTION = 'Thibault Le Ouay - Ducasse';
